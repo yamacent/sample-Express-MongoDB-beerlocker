bfd5d72149f78400baee45d82fbf6b252fd9f94e
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2015-04-04 15:49:47 +0900

    Add userId field to Beer model

--- controllers/beer.js
+++ controllers/beer.js
@@ -6,6 +6,7 @@ exports.postBeers = function (req, res) {
     beer.name = req.body.name;
     beer.type = req.body.type;
     beer.quantity = req.body.quantity;
+    beer.userId = req.user._id;
 
     beer.save(function (err) {
         if (err) { res.send(err); }
@@ -14,32 +15,28 @@ exports.postBeers = function (req, res) {
 };
 
 exports.getBeers = function (req, res) {
-    Beer.find(function (err, beers) {
+    Beer.find({ userId: req.user._id }, function (err, beers) {
         if (err) { res.send(err); }
         res.json(beers);
     });
 };
 
 exports.getBeer = function (req, res) {
-    Beer.findById(req.params.beer_id, function (err, beer) {
+    Beer.findById({ userId: req.user._id, _id: req.params.beer_id }, function (err, beer) {
         if (err) { res.send(err); }
         res.json(beer);
     });
 };
 
 exports.putBeer = function (req, res) {
-    Beer.findById(req.params.beer_id, function (err, beer) {
+    Beer.update({ userId: req.user._id, _id: req.params.beer_id }, { quantity: req.body.quantity }, function (err, num, raw) {
         if (err) { res.send(err); }
-        beer.quantity = req.body.quantity;
-        beer.save(function (err) {
-            if (err) { res.send(err); }
-            res.json(beer);
-        });
+        res.json({ message: num + ' updated' });
     });
 };
 
 exports.deleteBeer = function (req, res) {
-    Beer.findByIdAndRemove(req.params.beer_id, function (err) {
+    Beer.remove({ userId: req.user._id, _id: req.params.beer_id }, function (err) {
         if (err) { res.send(err); }
         res.json({ message: 'Beer removed from the locker!' });
     });
--- models/beer.js
+++ models/beer.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var BeerSchema = new mongoose.Schema({
     name: String,
     type: String,
-    quantity: Number
+    quantity: Number,
+    userId: String
 });
 
 module.exports = mongoose.model('Beer', BeerSchema);
